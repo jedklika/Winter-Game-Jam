244d94f7447dea8c5dff8e7b383041e2b6ec0366
jedklika <43482414+jedklika@users.noreply.github.com>
2019-01-02 23:42:42 -0500

    Increase speed
    Click to speed up

--- SkateBoarding game/Assets/Scripts/Movement.cs
+++ SkateBoarding game/Assets/Scripts/Movement.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Rigidbody rb;
+    
     public float SkateSpeed = 10f;
     public float PushSpeed = 5;
     bool Ongroud = true;
     bool Onboard = true;
     public GameObject Player;
+    public float rotationSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +20,13 @@ public class Movement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   float rotation = 0;
-        transform.Translate(Vector3.forward * PushSpeed * Time.deltaTime);
+    {
+        float rotation = 0f;
+        transform.Translate(0, 0, SkateSpeed* Time.deltaTime);
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Faster");
-            transform.Translate(Vector3.forward * SkateSpeed * Time.deltaTime);
-            Debug.Log("Go");
-        } 
+            transform.Translate(0, 0, PushSpeed * Time.deltaTime);
+        }
     }
 }
 
