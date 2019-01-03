37354f61ff5cf81b01a8f3cc0909ca3f2b743805
jedklika <43482414+jedklika@users.noreply.github.com>
2019-01-02 19:29:22 -0500

    movement in progress

---
+++ SkateBoarding game/Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float SkateSpeed = 10f;
+    public float PushSpeed = 5;
+    bool Ongroud = true;
+    bool Onboard = true;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   float rotation = 0;
+        transform.Translate(Vector3.forward * PushSpeed * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Faster");
+            transform.Translate(Vector3.forward * SkateSpeed * Time.deltaTime);
+            Debug.Log("Go");
+        } 
+    }
+}
+
