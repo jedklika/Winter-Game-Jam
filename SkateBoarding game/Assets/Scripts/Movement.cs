9b0e828f326dec00bac9f610b97e03df4e0333f5
jedklika <43482414+jedklika@users.noreply.github.com>
2019-01-03 05:34:07 -0500

    Jumping
    with bug that prevents jumping after a few times

--- SkateBoarding game/Assets/Scripts/Movement.cs
+++ SkateBoarding game/Assets/Scripts/Movement.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    
+
     public float SkateSpeed = 10f;
     public float PushSpeed = 5;
-    bool Ongroud = true;
-    bool Onboard = true;
+    public float JumpHeight;
+    bool onGround = true;
     public GameObject Player;
     public float rotationSpeed;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        onGround = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         float rotation = 0f;
-        transform.Translate(0, 0, SkateSpeed* Time.deltaTime);
-        if (Input.GetMouseButtonDown(0))
+        transform.Translate(0, 0, SkateSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             transform.Translate(0, 0, PushSpeed * Time.deltaTime);
+        }
+        if (onGround && Input.GetKeyUp(KeyCode.Space))
+        {
+            this.GetComponent<Rigidbody>().AddForce(Vector3.up * 350);
+            onGround = false;
+        }
+        }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+            Debug.Log("Hit");
         }
     }
 }
