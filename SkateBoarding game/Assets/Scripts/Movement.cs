0da36981714094e057ad1146552374f64b7cc5ef
jedklika <43482414+jedklika@users.noreply.github.com>
2019-01-05 05:05:13 -0500

    Tricks and Correct jump
    All tricks and a properly single working jump are in the game, next steps figure out how to get rotation and staring on the scoring mechanic.

--- SkateBoarding game/Assets/Scripts/Movement.cs
+++ SkateBoarding game/Assets/Scripts/Movement.cs
@@ -11,35 +11,98 @@ public class Movement : MonoBehaviour
     public GameObject Player;
     public float rotationSpeed;
     public float Sensitivity = 2f;
+    bool inAir;
+    bool onRail;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        onGround = true;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (onGround == true)
-        {
-            transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * Time.deltaTime * Sensitivity);
-        }
+        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * Time.deltaTime * Sensitivity);
         float rotation = 0f;
         transform.Translate(0, 0, SkateSpeed * Time.deltaTime);
         if (onGround && Input.GetMouseButton(0))
         {
             transform.Translate(0, 0, PushSpeed * Time.deltaTime);
-            onGround = true;
+            
         }
-        if (onGround && Input.GetKeyDown(KeyCode.Space))
+        if (onGround && Input.GetMouseButton(1))
         {
-            this.GetComponent<Rigidbody>().AddForce(Vector3.up * 250);
+                this.GetComponent<Rigidbody>().AddForce(Vector3.up * 400);
             onGround = false;
+            inAir = true;
+            
         }
-        else
+        if(transform.position.y >= 4.9){
+            onGround = false;
+            inAir = true;
+        }
+        if(inAir && Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("KickFlip");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("HeelFlip");
+        }
+        if (onGround && Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Mannual");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("Impossible");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.M))
+        {
+            Debug.Log("Melon");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("TailGrab");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("Pop It shove it");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log("indy");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("Nosegrab");
+        }
+        if (onRail && Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("crooked");
+        }
+        if (onRail && Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Smith");
+        }
+
+    }
+     void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
         {
             onGround = true;
+            inAir = false;
+            onRail = false;
+        }
+        if (other.gameObject.CompareTag("Rail"))
+        {
+            onGround = true;
+            inAir = false;
+            onRail = true;
+            Debug.Log("Grind");
         }
     }
 }
