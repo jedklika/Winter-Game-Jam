4708851b06fd3b4be8326f6ab539621cc39345ca
jedklika <43482414+jedklika@users.noreply.github.com>
2019-01-05 23:49:41 -0500

    Revert "Tricks updated"
    This reverts commit bd74ff95ea6943d19aa7c9b36e5af29921ab9791.

--- SkateBoarding game/Assets/Scripts/Movement.cs
+++ SkateBoarding game/Assets/Scripts/Movement.cs
@@ -13,13 +13,12 @@ public class Movement : MonoBehaviour
     public float Sensitivity = 2f;
     bool inAir;
     bool onRail;
-    public float MannualSpeed = 10f;
- 
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
@@ -31,85 +30,80 @@ public class Movement : MonoBehaviour
         if (onGround && Input.GetMouseButton(0))
         {
             transform.Translate(0, 0, PushSpeed * Time.deltaTime);
-
+            
         }
         if (onGround && Input.GetMouseButton(1))
         {
-            this.GetComponent<Rigidbody>().AddForce(Vector3.up * 400);
+                this.GetComponent<Rigidbody>().AddForce(Vector3.up * 400);
             onGround = false;
             inAir = true;
-
+            
         }
-        if (transform.position.y >= 4.9) {
+        if(transform.position.y >= 4.9){
             onGround = false;
             inAir = true;
         }
-        if (inAir && Input.GetKeyDown(KeyCode.F) && Input.GetKey(KeyCode.Keypad6))
+        if(inAir && Input.GetKeyDown(KeyCode.K))
         {
             Debug.Log("KickFlip");
         }
-            if (inAir && Input.GetKeyDown(KeyCode.F) && Input.GetKey(KeyCode.Keypad4))
-            {
-                Debug.Log("HeelFlip");
-            }
-            if (onGround && Input.GetKey(KeyCode.Space))
-            {
-                Debug.Log("Mannual");
-                transform.Translate(0, 0, -MannualSpeed * Time.deltaTime);
-            }
-            if (inAir && Input.GetKeyDown(KeyCode.F) && Input.GetKey(KeyCode.Keypad8))
-            {
-                Debug.Log("Impossible");
-            }
-            if (inAir && Input.GetKeyDown(KeyCode.G) && Input.GetKey(KeyCode.Keypad4))
-            {
-                Debug.Log("Melon");
-            }
-            if (inAir && Input.GetKeyDown(KeyCode.G) && Input.GetKey(KeyCode.Keypad2))
-            {
-                Debug.Log("TailGrab");
-            }
-            if (inAir && Input.GetKeyDown(KeyCode.F) && Input.GetKey(KeyCode.Keypad2))
-            {
-                Debug.Log("Pop It shove it");
-            }
-            if (inAir && Input.GetKeyDown(KeyCode.G) && Input.GetKey(KeyCode.Keypad6))
-            {
-                Debug.Log("indy");
-            }
-            if (inAir && Input.GetKeyDown(KeyCode.G) && Input.GetKey(KeyCode.Keypad8))
-            {
-                Debug.Log("Nosegrab");
-            }
-            if (onRail && Input.GetKeyDown(KeyCode.G))
-            {
-                Debug.Log("crooked");
-            }
-            if (onRail && Input.GetKeyDown(KeyCode.F))
-            {
-                Debug.Log("Smith");
-            }
-            if (onRail && Input.GetKey(KeyCode.Space))
-            {
-                Debug.Log("50-50");
-            }
-
-        }
-        void OnCollisionEnter(Collision other)
+        if (inAir && Input.GetKeyDown(KeyCode.H))
         {
-            if (other.gameObject.CompareTag("Ground"))
-            {
-                onGround = true;
-                inAir = false;
-                onRail = false;
-                
-            }
-            if (other.gameObject.CompareTag("Rail"))
-            {
-                onGround = true;
-                inAir = false;
-                onRail = true;
-                Debug.Log("Grind");
-            }
+            Debug.Log("HeelFlip");
+        }
+        if (onGround && Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Mannual");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("Impossible");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.M))
+        {
+            Debug.Log("Melon");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("TailGrab");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("Pop It shove it");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log("indy");
+        }
+        if (inAir && Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("Nosegrab");
+        }
+        if (onRail && Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("crooked");
+        }
+        if (onRail && Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Smith");
+        }
+
+    }
+     void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+            inAir = false;
+            onRail = false;
+        }
+        if (other.gameObject.CompareTag("Rail"))
+        {
+            onGround = true;
+            inAir = false;
+            onRail = true;
+            Debug.Log("Grind");
         }
     }
+}
+
